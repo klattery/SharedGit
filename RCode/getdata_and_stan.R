34dc4af996d4b345be758dce61fa9ac063651fbf
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2021-10-18 12:54:47 -0500

    Update getdata_and_stan.R

--- RCode/getdata_and_stan.R
+++ RCode/getdata_and_stan.R
@@ -3,13 +3,13 @@ if (!file.exists(file.path(dir_data, my_csv_file))){
   message(paste0("Cannot find your data file: ", my_csv_file))    
 } else {
   data_in <- read.csv(file.path(dir_data, my_csv_file), as.is=TRUE)
-  message("Read data into R file data_in")
+  message("Read data into R file: data_in")
   str(data_in)
 }
 if (!file.exists(file.path(dir_stan, my_stan_code))){
   message(paste0("Cannot find Stan Code in: ", dir_stan))    
 } else {
-  cat("Compiling Stan code...")
+  message("Compiling Stan code...")
   HB_model <- cmdstan_model(file.path(dir_stan,my_stan_code), quiet = TRUE, cpp_options = list(stan_threads = TRUE))
-  cat("DONE")
+  message("DONE")
 }
